2ba4ef40fc2a56ce1efc8786ea0184778a271775
Javier Moreno <javier.moreno@smallplanet.com>
2017-03-08 12:42:36 -0500

    Basic functions for fragments management.

--- app/src/main/java/com/mdscrn/hackernewsclient/MainActivity.kt
+++ app/src/main/java/com/mdscrn/hackernewsclient/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mdscrn.hackernewsclient
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            //changeFragment(NewsFragment())
+        }
+    }
+
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction()
+        if (cleanStack) {
+            clearBackStack()
+        }
+
+        ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.replace(R.id.activity_base_content, f)
+        ft.addToBackStack(null)
+        ft.commit()
+    }
+
+    fun clearBackStack() {
+        val manager = supportFragmentManager
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0)
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
     }
 }
