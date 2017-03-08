13b86aaec7bc84f286c3adeba062cab7b5e53e07
Javier Moreno <javier.moreno@smallplanet.com>
2017-03-08 13:29:25 -0500

    Using synthetic properties to access and modify RecycleView.

--- app/src/main/java/com/mdscrn/hackernewsclient/NewsFragment.kt
+++ app/src/main/java/com/mdscrn/hackernewsclient/NewsFragment.kt
@@ -3,10 +3,12 @@ package com.mdscrn.hackernewsclient
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mdscrn.hackernewsclient.commons.inflate
+import kotlinx.android.synthetic.main.fragment_news.*
 
 
 /**
@@ -15,6 +17,11 @@ import com.mdscrn.hackernewsclient.commons.inflate
 class NewsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return container?.inflate(R.layout.fragment_news)
+        val view = container?.inflate(R.layout.fragment_news)
+
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 }
