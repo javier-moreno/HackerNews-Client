ca4969438d22594fd9d519dee57de0ac62e0c619
Javier Moreno <javier.moreno@smallplanet.com>
2017-03-10 15:17:22 -0500

    Refactoring.

--- app/src/main/java/com/mdscrn/hackernewsclient/news/newsdelegatesadapters/NewsDelegateAdapter.kt
+++ app/src/main/java/com/mdscrn/hackernewsclient/news/newsdelegatesadapters/NewsDelegateAdapter.kt
@@ -3,8 +3,8 @@ package com.mdscrn.hackernewsclient.news.newsdelegatesadapters
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.mdscrn.hackernewsclient.R
-import com.mdscrn.hackernewsclient.commons.inflate
 import com.mdscrn.hackernewsclient.api.data.HackerNewsItem
+import com.mdscrn.hackernewsclient.commons.inflate
 import kotlinx.android.synthetic.main.news_item.view.*
 
 /**
